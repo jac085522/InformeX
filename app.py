21f2b425e9508d99d90e189ebaa943e04128a271
jac085522 <jac085522@gmail.com>
2025-06-08 14:39:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ RUTA_PLANTILLA = os.path.join(BASE_DIR, 'plantilla.docx')
 
 @app.route('/')
 def formulario():
-    return render_template('e_vehiculo.html')
+    return render_template('index.html')
 
 @app.route('/generar', methods=['POST'])
 def generar():
